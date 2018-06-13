036e22960dd374d9e489ee185fa4634b6f9e1a5a
Lu Luo <lu.luo@sap.com>
2018-06-12 22:38:53 -0700

    Rename the variable to make it readable.+

--- netty4-demos/src/main/java/com/waylau/netty/demo/discard/DiscardServer.java
+++ netty4-demos/src/main/java/com/waylau/netty/demo/discard/DiscardServer.java
@@ -1,7 +1,6 @@
 package com.waylau.netty.demo.discard;
 
 import io.netty.bootstrap.ServerBootstrap;
-
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
@@ -25,18 +24,20 @@ public class DiscardServer {
     EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
     EventLoopGroup workerGroup = new NioEventLoopGroup();
     try {
-      ServerBootstrap b = new ServerBootstrap(); // (2)
-      b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class) // (3)
+      ServerBootstrap bootstrap = new ServerBootstrap(); // (2)
+      bootstrap.group(bossGroup, workerGroup)
+          .channel(NioServerSocketChannel.class) // (3)
           .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
             @Override
             public void initChannel(SocketChannel ch) throws Exception {
+              System.out.println("Initialize a new connection: " + ch);
               ch.pipeline().addLast(new DiscardServerHandler());
             }
           }).option(ChannelOption.SO_BACKLOG, 128) // (5)
           .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
 
       // 绑定端口，开始接收进来的连接
-      ChannelFuture f = b.bind(port).sync(); // (7)
+      ChannelFuture f = bootstrap.bind(port).sync(); // (7)
 
       // 等待服务器 socket 关闭 。
       // 在这个例子中，这不会发生，但你可以优雅地关闭你的服务器。
